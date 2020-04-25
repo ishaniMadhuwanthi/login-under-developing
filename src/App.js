8a62203b027dcf57a60f1852355c1e507f3242ea
RoshaniDilhara <roshanidilhara7@gmail.com>
2020-04-26 05:07:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer,  Content} from 'react-mdl';
+import Main from './components/main';
+import {Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/projects">Projects</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+            <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/projects">Projects</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+
+
+  );
+}
+
+export default App;
